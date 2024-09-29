67c906d4a667e9fbda79feb6d4f5c0bc551a2e8a
Rouguiatou <131063469+Rougui224@users.noreply.github.com>
2024-09-29 23:53:34 +0200

    Delete inscriptionPOO directory

--- inscriptionPOO/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    require_once('./controllers/controller.php');
-    AddUser();
-    require_once('views/viewHome.php');
--- inscriptionPOO/models/user/Securite.php
+++
@@ -1,6 +0,0 @@
-<?php
-    class Securite {
-        public static function hash($password){
-            return "1D94JD".sha1($password.'djndf').'fsjdn';
-        }
-    }
